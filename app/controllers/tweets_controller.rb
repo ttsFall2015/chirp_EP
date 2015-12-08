0a879a0da921d7aa32770eaf179b50c379d2f489
Paul <pwh041809@gmail.com>
2015-12-07 21:02:20 -0500

    controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,10 @@
 class TweetsController < ApplicationController
+ before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
+ before_filter :authenticate_user!
+
   def index
+    @tweets = Tweet.all
   end
 
   def show
@@ -9,6 +14,27 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
   end
 
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html {redirect_to @tweet, notice: 'Tweet was successfully created.'}
+      else
+        format.html {render :new}
+      end
+    end
+  end
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:message, :user_id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 end
